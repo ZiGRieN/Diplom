6a69322414a74b4fc2f4493aa058702bb7cecc96
ZiGRieN <dante7373d@yandex.ru>
2017-01-17 10:48:08 +0300

    v1.0
    Server

---
+++ Project001/Project001/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project001
+{
+    class Client
+    {
+        public Client(TcpClient client)
+        {
+            Console.WriteLine(client.Client.RemoteEndPoint);
+        }
+    }
+}
---
+++ Project001/Project001/Server.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Project001
+{
+    class Server
+    {
+
+    Thread thread;
+        
+        public void StartServer(int port)
+        {
+            
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            ThreadPool.SetMaxThreads(4,4);
+            ThreadPool.SetMinThreads(1,1);
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                thread = new Thread(new ParameterizedThreadStart(ClientThread));
+                thread.Start(client);
+            }
+
+        }
+
+        static void ClientThread(Object StateInfo)
+        {
+            new Client((TcpClient)StateInfo);
+        }
+    }
+}
